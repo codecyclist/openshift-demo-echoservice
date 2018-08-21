88c8d53a966bbd9aa13ca5019069b113cc6623e8
Manfred Dreese <md@dreese.de>
2018-08-22 00:10:07 +0200

    chg: Introduded Lombok

--- src/main/java/de/m9d/demos/openshiftechoservice/domain/Message.java
+++ src/main/java/de/m9d/demos/openshiftechoservice/domain/Message.java
@@ -1,15 +1,15 @@
 package de.m9d.demos.openshiftechoservice.domain;
 
+import lombok.Data;
 import org.hibernate.validator.constraints.Length;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import java.time.Instant;
 
-
+@Data
 public class Message {
-    private Instant created;
+    private Instant created = Instant.now();
 
     @NotNull(message="{validation.newMessage.empty.message}")
     @NotEmpty(message="{validation_newMessage_empty_message}")
@@ -17,24 +17,6 @@ public class Message {
     private String content;
 
     public Message(String content) {
-        this.created = Instant.now();
-        this.content = content;
-    }
-
-    public Message() {
-        this.created = Instant.now();
-    }
-
-
-    public Instant getCreated() {
-        return created;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
         this.content = content;
     }
 }
